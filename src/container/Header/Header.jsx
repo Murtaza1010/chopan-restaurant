1e6b22a784c9f111517458dd9be6cf397b2a6440
MK <mortezakarim0@gmail.com>
2023-06-30 15:49:56 +0100

    "about us section created and styled" "header section: image not responsive"

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => (
       <SubHeading title="Chase The New Flavour"/> 
       <h1 className='app__header-h1'>The Key To Fine Dinning</h1>
       <p className='p__opensans' style={{margin: '2rem 0rem'}}>We create magic for all ages with our mouth smacking foods and swiftest restaurant takeaway service. We assure you to get the best possible service from each of our staff members.</p>
-      <button type='button' className='custom_button'>Explore Menu</button>
+      <button type='button' className='custom__button'>Explore Menu</button>
 
     </div>
-       <div className='app__wrapper_img'>
-      <img src={images.welcome} alt='header img'/>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img' />
     </div>
    
     
